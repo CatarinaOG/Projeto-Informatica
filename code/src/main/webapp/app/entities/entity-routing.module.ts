e6ffb67059d416172d6eb25db63467e6b7851948
Catarina Gonçalves <catarina.oliveira.41@hotmail.com>
2023-10-10 21:09:34 +0100

    dependencias extra

--- code/src/main/webapp/app/entities/entity-routing.module.ts
+++ code/src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,11 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'region',
+        data: { pageTitle: 'Regions' },
+        loadChildren: () => import('./region/region.module').then(m => m.RegionModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
