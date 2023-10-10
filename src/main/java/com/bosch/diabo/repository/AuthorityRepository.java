63c7d231a6c2ec520a3b1de5cbc631bed2eee79c
Catarina Gonçalves <catarina.oliveira.41@hotmail.com>
2023-10-10 20:25:02 +0100

    estrutura base

---
+++ src/main/java/com/bosch/diabo/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.bosch.diabo.repository;
+
+import com.bosch.diabo.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
