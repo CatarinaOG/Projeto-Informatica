d14cea15460fd7156cecf8d54570eb112d0e4b0b
Catarina Gonçalves <catarina.oliveira.41@hotmail.com>
2023-11-10 16:42:07 +0000

    Initial version of diabo generated by generator-jhipster@7.9.4

---
+++ code2/src/main/java/com/bosch/diabo/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.bosch.diabo.service.dto;
---
+++ code2/src/test/java/com/bosch/diabo/config/SqlTestContainer.java
@@ -0,0 +1,9 @@
+package com.bosch.diabo.config;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+
+public interface SqlTestContainer extends InitializingBean, DisposableBean {
+    JdbcDatabaseContainer<?> getTestContainer();
+}
